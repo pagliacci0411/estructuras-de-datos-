891462aa204c8152ca65b5d90c3cf8fda9583d1f
pagliacci0411 <31490035+pagliacci0411@users.noreply.github.com>
2018-04-18 17:32:21 -0500

    Update tree.java

--- tree.java
+++ tree.java
@@ -1,11 +1,17 @@
-/**
- * 
- */
+		/**arboles n arios n = cantidad maxima de hijos que puede tener el arbol
+		 * profundidad := distanciia entre la raiz y el nodo mas abajo 
+		 * altura  :=distancia del padre al desendiente más abajo
+		 * todos los de altura 0 son hojas
+		 */
+		/**
+		 * arboles binarios = maximo 2 hijos
+		 * math expresion tree
+		 * pre order
+		 * post order
+		 * in order
+		 */
+		
 
-/**
- * @author Estudiante
- *
- */
 public class tree {
 
 }
