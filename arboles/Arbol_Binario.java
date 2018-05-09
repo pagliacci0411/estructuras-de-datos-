e8e45aaaf5ff777c63287a587fb323d3677cdf19
pagliacci0411 <31490035+pagliacci0411@users.noreply.github.com>
2018-05-09 16:32:14 -0500

    Update Arbol_Binario.java

--- arboles/Arbol_Binario.java
+++ arboles/Arbol_Binario.java
@@ -11,6 +11,19 @@ public class Arbol_Binario {
 	
 	
 	public void insertar (int valor)
+	{package asfddsafdsf;
+
+public class Arbol_Binario {
+	
+	nodo root = null;
+	
+	public Arbol_Binario () 
+	{
+		
+	}
+	
+	
+	public void insertar (int valor)
 	{
 		nodo node = new nodo (valor);
 		if (root  == null) 
@@ -72,6 +85,121 @@ public class Arbol_Binario {
 		System.out.println(node.tostring());
 		inorder(node.der);
 		}
+	}
+	public void delete (int a) 
+	{
+		nodo temp = root;
+		nodo padre = null;
+		boolean left = false;
+		
+
+		while (temp!= null)
+		{
+			if (temp.value == a)
+				break;
+			else 
+			{
+				padre = temp ;
+				if (a < temp.value)
+				{
+					temp = temp.izq;
+				left = true;
+				}
+				else
+				{
+					temp = temp.der;
+					left = false;
+				}	
+			}
+		}
+		if (temp!= null)
+		{
+			int contador = this.offspring(temp);
+			if(contador == 0)
+			{
+				if (left)
+					padre.izq = null;
+				else 
+					padre.der = null;
+			}
+			else
+			{
+					if (contador ==1)
+					{
+						 if ( temp.izq != null)
+						 {
+							 if(left)
+								 padre.izq = temp.izq;
+							 else 
+								 padre.der = temp.izq;
+							 
+						 }
+						 else
+							 if(left)
+								 padre.izq = temp.der;
+							 else 
+								 padre.der = temp.der;						 
+					}	
+					else 
+					{/*
+						if(padre.der == null)
+						{
+						while (temp.der.izq != null )
+						 {
+							 temp.der.izq = temp.der.izq.izq;
+						 }
+						 padre.izq = temp.der.izq;
+						}
+						else 
+						{
+							while (temp.der.izq != null )
+							 {
+								 temp.der.izq = temp.der.izq.izq;
+							 }
+							 padre.izq = temp.der.izq;
+						}*/
+					nodo less = temp.der;
+					nodo less_padre = less;
+					while (less.izq != null)
+					{
+					less_padre = less;
+						less = less.izq;
+					}
+					
+					if(less.der != null)
+					{
+						less_padre.izq = less.der;
+					}
+					
+					less.der = temp.der;
+					less.izq = temp.izq;
+					
+					/// hay un error en un caso 					
+					if(left)
+					padre.izq = less;
+					else
+						padre.der = less;
+					
+					}	
+			}
+		}
+	}
+	
+	
+	public nodo buscar(int a)
+	{
+		nodo temp = root;
+		while (temp!= null)
+			if (temp.value == a)
+				return temp;
+			else 
+				if (a < temp.value)
+					temp = temp.izq;
+				else
+					temp = temp.der;
+			
+		
+		return null;
 	}
 	
 	
