80dd3442df67215592ce23fdc07899ed72e72e99
pagliacci0411 <31490035+pagliacci0411@users.noreply.github.com>
2018-06-08 11:23:47 -0500

    Update complete.java

--- arboles/complete.java
+++ arboles/complete.java
@@ -1,5 +1,46 @@
 package arbol_avl;
 
-public class complete {
+public class complete 
+{
+
+		public Complete()
+		{
+			
+		}
+		
+		public arbol_binario in_PreToPost(String pre, String in)
+		{
+			if(in.length() == 0)
+				return new arbol_binario();	
+			else
+			{	
+				arbol_binario  temp = new arbol_binario();
+				if(in.length() == 1)
+					temp.root = new Node(in);
+				else
+				{	
+					int[] Arr = new int[in.length()];
+				
+					for(int i = 0; i < in.length() ; i++)
+						Arr[i] = pre.indexOf(in.substring(i,i+1));
+				
+					int min = Integer.MAX_VALUE;
+					int index = -1;
+					for(int i = 0; i < Arr.length ; i++)
+						if(Arr[i] < min)
+						{	
+							min = Arr[i];
+							index = i;
+						}
+					String Dw = in.substring(0, index);
+					String Up = in.substring(index + 1, in.length());
+				
+					temp.root = new Node(in.charAt(index) + "");
+					temp.root.left = this.in_PreToPost(pre, Dw).root;
+					temp.root.right = this.in_PreToPost(pre, Up).root;
+				}	
+				return temp;
+			}
+		}
 
 }
